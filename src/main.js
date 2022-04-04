38f20bd36bfbce9b875df61134eb0d07855fe9cc
Cong Duy <rudbich199@gmail.com>
2022-04-04 10:16:44 +0700

    excercise-4

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
+
+Vue.filter('formatDate', function (date) {
+    const arr = date.split("-");
+    arr[0] = "Ngày " + arr[0];
+    arr[1] = "Tháng " + arr[1];
+    arr[2] = "Năm " + arr[2];
+
+    return arr.join(" ");
+})
+
+Vue.filter('toCurrency', function (value) {
+    if (typeof value !== "number") {
+        return value;
+    }
+    let formatter = new Intl.NumberFormat('ja-JP', {
+        style: 'currency',
+        currency: 'JPY'
+    });
+    return formatter.format(value);
+});
